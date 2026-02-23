b9caa59f9e1ae23651165f8c56be2d1f2e8f9f67
mad-cat-lon <113548315+mad-cat-lon@users.noreply.github.com>
2026-02-22 19:12:46 -0500

    chore: remove unnecessary from future import

--- src/heretic/evaluator.py
+++ src/heretic/evaluator.py
@@ -1,8 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2025-2026  Philipp Emanuel Weidmann <pew@worldwidemann.com> + contributors
 
-from __future__ import annotations
-
 from typing import Any
 
 from optuna.study import StudyDirection
